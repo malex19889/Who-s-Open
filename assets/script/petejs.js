c74901bf6bb6e50efff7c7a5f35674d4ff195b8c
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-09-02 20:54:47 -0500

    added markers

--- assets/script/petejs.js
+++ assets/script/petejs.js
@@ -3,74 +3,27 @@ $(document).ready(function(){
 
 
 
- /* $("#search-button").on("click", function(){
-
-  mapData();
-
-  });
-
-
-
-
-function mapData(){
-
-
-var queryURL = "http://api.ipstack.com/66.25.134.187?access_key=b4ea346fbe0daf6aaabebc950e3cc76a";
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-
-  console.log(response);
-
-  var
-
-
-  });
-};*/
-createMap();
-
-var x = $("#search-button");
-
-$("#search-button").on("click", function () {
-
   getLocation();
-  console.log("inside click");
-
-})
-
-
-function getLocation() {
-
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-function showPosition(position) {
-  var lng = position.coords.longitude;
-  var lat = position.coords.latitude;
-   
+  mapboxgl.accessToken = 'pk.eyJ1IjoieHhtYWt2ZWxpMjJ4eCIsImEiOiJja2VjNmhhaGcwNGtuMnVrZWdkNXprZjJnIn0.gmnmTgSzzlIYiQGGCePE3w';
   
-displayLocation(lat,lng);
-  console.log( "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude)
-}
-
-
-function displayLocation(lat, lng){
-
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/xxmakveli22xx/ckejbvql12rye19pmbaa3b4nl',
+    center: [-96, 37.8],
+    zoom: 3
+  });
+  
+  
+  // code from the next step will go here!
+  function displayLocation(lng, lat){
+  console.log("inside function ");
   var geojson = {
     type: 'FeatureCollection',
     features: [{
       type: 'Feature',
       geometry: {
         type: 'Point',
-        coordinates: [-77.032, 38.913]
+        coordinates: [lng, lat]
       },
       properties: {
         title: 'Mapbox',
@@ -89,33 +42,46 @@ function displayLocation(lat, lng){
       }
     }]
   };
-
-// add markers to map
-geojson.features.forEach(function(marker) {
-
+  
+  
+  // add markers to map
+  geojson.features.forEach(function(marker) {
+  
   // create a HTML element for each feature
   var el = document.createElement('div');
   el.className = 'marker';
-
+  
   // make a marker for each feature and add to the map
   new mapboxgl.Marker(el)
     .setLngLat(marker.geometry.coordinates)
     .addTo(map);
-});
+  });
   
+  };
+  
+  
+  function getLocation() {
+  
+  
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  function showPosition(position) {
+    var lng = position.coords.longitude;
+    var lat = position.coords.latitude;
+     
+    
+  displayLocation(lng,lat);
+    console.log( "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude)
+  }
 
-};
 
 
-function createMap(){
-mapboxgl.accessToken = 'pk.eyJ1IjoieHhtYWt2ZWxpMjJ4eCIsImEiOiJja2VjNmhhaGcwNGtuMnVrZWdkNXprZjJnIn0.gmnmTgSzzlIYiQGGCePE3w';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/xxmakveli22xx/ckejbvql12rye19pmbaa3b4nl', // stylesheet location
-    center: [-96, 37.8], // starting position [lng, lat]
-    zoom: 3 // starting zoom
-});
-}
+
 
 
 
