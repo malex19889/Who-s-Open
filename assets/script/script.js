4d2755ca0895da1d2000a2e6ea74b8a87b057061
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-09-09 10:45:09 -0500

    added function to display all locations

--- assets/script/script.js
+++ assets/script/script.js
@@ -37,6 +37,7 @@ $("#search").on("click", function () {
   place = $("#place").val();
   console.log(place)
   placesCall();
+  
 })
 // google places API call
 function placesCall() {
@@ -106,7 +107,10 @@ function renderInfo(){
         } else {
           console.log(placesInfo.results[i])
         }
-      }    
+      }  
+      //this my function to display the locations on the map
+      displayResults();
+      
 }
 // call to places details from click
 function placesDeetsCall() {
@@ -164,5 +168,121 @@ function modalRender(){
 getLocation();
 
 
+
 // render target id info 
 // "+lat+","+lon+"
+
+
+//My Code delete if it doesn't work 
+
+
+
+       
+
+
+
+
+  
+  mapboxgl.accessToken = "pk.eyJ1IjoieHhtYWt2ZWxpMjJ4eCIsImEiOiJja2VjNmhhaGcwNGtuMnVrZWdkNXprZjJnIn0.gmnmTgSzzlIYiQGGCePE3w";
+  var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/xxmakveli22xx/ckejbvql12rye19pmbaa3b4nl',
+  center: [-98.6087424, 29.501030399999998],
+  zoom: 14
+});
+
+  
+
+        
+         
+       
+     
+
+ function displayLocation(lng, lat){
+
+   var geojson = {
+     type: 'FeatureCollection',
+     features: [{
+        type: 'Feature',
+        geometry: {
+         type: 'Point',
+         coordinates: [lng, lat]
+       },
+       properties: {
+         title: 'City',
+         description: 'San Antonio, Texas'
+       }
+     
+   }]
+     
+   };
+     
+
+   // add markers to map
+geojson.features.forEach(function(marker) {
+
+ // create a HTML element for each feature
+ var el = document.createElement('div');
+     el.className = 'marker';
+ 
+ // make a marker for each feature and add to the map
+ new mapboxgl.Marker(el)
+   .setLngLat(marker.geometry.coordinates)
+   .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+   .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+   .addTo(map);
+ });
+
+
+
+
+
+
+ };
+
+ function displayResults(){
+    
+      for(var i = 0; i < latArr.length; i++){
+        console.log("This array is Lat "+ latArr[i]+ " Long "+ lonArr[i]);
+
+        displayLocation(lonArr[i], latArr[i]); 
+      
+      
+      
+      }   
+
+
+ }
+ 
+
+ 
+ 
+ 
+ //this function will grab the geo location
+ function getLocation() {
+ 
+ 
+   if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        }else { 
+             x.innerHTML = "Geolocation is not supported by this browser.";
+           }
+      }
+  
+    function showPosition(position) {
+         var lng = position.coords.longitude;
+         var lat = position.coords.latitude;
+         displayLocation(lng,lat);
+        }
+
+
+// Add geolocate control to the map.
+     map.addControl(
+         new mapboxgl.GeolocateControl({
+         positionOptions: {
+         enableHighAccuracy: true
+          },
+         trackUserLocation: true
+         })
+      );
+ 
